91d60e6acff33aa24a6f089ced56c875ef6fbfc5
abdirahman abdi ali <abdulrahmanibnimohammad@gmail.com>
2025-03-07 19:52:17 +0300

    first-class-functions

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function receivesAFunction(callback) {
+    callback()
+  }
+  
+  function returnsANamedFunction() {
+    return function named() {
+      console.log("The girl is Arya.")
+    }
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+      console.log('The girl has no name.')
+    }
+  }
+  
